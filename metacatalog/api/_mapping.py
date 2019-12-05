2764b186fdc69946ab1eec1ec223c83c6ddcc3a6
Mirko Mälicke <mirko@maelicke-online.de>
2019-12-05 15:43:29 +0100

    more mappings

--- metacatalog/api/_mapping.py
+++ metacatalog/api/_mapping.py
@@ -12,3 +12,19 @@ TABLE_MAPPING = dict(
     units=models.Unit,
     variables=models.Variable
 )
+
+ENTITY_MAPPING = dict(
+    datasourcetype=models.DataSource,
+    datasource_type=models.DataSourceType,
+    datasource=models.DataSource,
+    entry=models.Entry,
+    keyword=models.Keyword,
+    license=models.License,
+    person=models.Person,
+    author=models.Person,
+    contributor=models.Person,
+    person_role=models.PersonRole,
+    personrole=models.PersonRole,
+    unit=models.Unit,
+    variable=models.Variable
+)
