7e827f586cd63eb8bfe258a7b926bf9cefb2bf29
Mirko Mälicke <mirko@maelicke-online.de>
2019-12-05 15:43:12 +0100

    added more data types

---
+++ metacatalog/models/generic_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Integer, Numeric
+
+from metacatalog.db.base import Base
+
+
+class DataPoint(Base):
+    __tablename__ = 'generic_1d_data'
+
+    # columns
+    entry_id = Column(Integer, ForeignKey('entries.id'), primary_key=True)
+    index = Column(Numeric, primary_key=True)
+    value = Column(Numeric, nullable=False)
+
+    @classmethod
+    def is_valid_data_series(cls, data):
+        return isinstance(data, (pd.DataFrame, pd.Series)) and len(data.columns)
+
+
+class DataPoint2D(Base):
+    __tablename__ = 'generic_2d_data'
+
+    # columns
+    entry_id = Column(Integer, ForeignKey('entries.id'), primary_key=True)
+    index = Column(Numeric, primary_key=True)
+    value1 = Column(Numeric, nullable=False)
+    value2 = Column(Numeric, nullable=False)
+
+    @classmethod
+    def is_valid_data_series(cls, data):
+        return isinstance(data, pd.DataFrame) and len(data.columns) == 2
---
+++ metacatalog/models/geometry_data.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Integer, DateTime
+from geoalchemy2 import Geometry
+
+from metacatalog.db.base import Base
+
+
+class GeometryTimeseries(Base):
+    __tablename__ = 'geom_timeseries'
+
+    # columns
+    entry_id = Column(Integer, ForeignKey('entries.id'), primary_key=True)
+    tstamp = Column(DateTime, primary_key=True)
+    geom = Column(Geometry, nullable=False)
+    srid = Column(Integer)
+
+
+class GenericGeometryData(Base):
+    __tablename__ = 'geneic_geometry_data'
+
+    # columns
+    entry_id = Column(Integer, ForeignKey('entries.id'), primary_key=True)
+    index = Column(Integer, primary_key=True)
+    geom = Column(Geometry, nullable=False)
+    srid = Column(Integer)
