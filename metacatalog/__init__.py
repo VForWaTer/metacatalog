df6d5a03bd64901c67ccecf579f22333209b81ce
Mirko Mälicke <mirko@maelicke-online.de>
2019-12-05 15:44:57 +0100

    increased version number

--- metacatalog/__init__.py
+++ metacatalog/__init__.py
@@ -4,7 +4,7 @@ import os
 #from .db.session import get_session
 #from .models import *
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Mirko Mälicke'
 BASEPATH = os.path.abspath(os.path.dirname(__file__))
 DATAPATH = os.path.join(BASEPATH, 'data')
