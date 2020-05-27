8e70101a73a6df3fcb31defaaff3c0ca5754866a
Mirko Mälicke <mirko.maelicke@kit.edu>
2020-05-27 12:32:55 +0200

    Update VERSION

--- metacatalog/__init__.py
+++ metacatalog/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__ = 'Mirko Mälicke'
 BASEPATH = os.path.abspath(os.path.dirname(__file__))
 DATAPATH = os.path.join(BASEPATH, 'data')
