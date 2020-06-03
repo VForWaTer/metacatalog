ebd3b3b879ee84773924d1fe55f627e3e93e17d2
Mirko Mälicke <mirko@maelicke-online.de>
2020-06-03 13:07:13 +0200

    set new Version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Mirko Mälicke'
 author = 'Mirko Mälicke <mirko.maelicke@kit.edu>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
