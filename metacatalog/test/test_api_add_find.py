7509f06cf02b5d7f1cd27f1262b2d668f2598740
Mirko Mälicke <mirko@hydrocode.de>
2021-05-06 11:25:41 +0200

    trying to dix test

--- metacatalog/test/test_api_add_find.py
+++ metacatalog/test/test_api_add_find.py
@@ -262,7 +262,6 @@ def add_details(session):
 
      # add nested details
     e2.add_details(
-        session,
         foo=dict(bar=['list', 'of', 'strings'], baz=42),
         answer=42,
         commit=True
