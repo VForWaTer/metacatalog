07652b94800a77b5b7bb9cd29fb7f3a15e348485
AlexDo1 <alex.dolich@gmail.com>
2022-11-29 10:31:26 +0100

    location view (sql)

--- metacatalog/db/views/locations.sql
+++ metacatalog/db/views/locations.sql
@@ -1,10 +1,51 @@
-CREATE OR REPLACE VIEW locations AS
-SELECT entries.id,
-	entries.location,
-	spatial_scales.extent,
-	ST_Envelope(spatial_scales.extent) "bbox",
-	ST_Centroid(spatial_scales.extent) "centroid",
-	ST_Area(spatial_scales.extent :: geography) "area_sqm"
+--CREATE OR REPLACE VIEW locations AS
+select 
+	st_asewkt(t.point_location),
+	t.*,
+	st_area(t.geom::geography) as "area_sqm"
+from 
+(SELECT 
+entries.id,
+	case when entries.location is not null then
+		entries.location
+	else 
+		case when spatial_scales.extent is not null then
+			st_centroid(spatial_scales.extent)
+		else 
+ 			case when entries.geom is not null then
+ 				st_centroid(entries.geom)
+ 			else
+				null
+ 			end
+		end
+	end  
+	as point_location
+	,
+ 
+ 	case when entries.geom is not null then
+ 		st_envelope(entries.geom)
+ 	else
+		case when spatial_scales.extent is not null then
+			st_envelope(spatial_scales.extent)
+		else 
+			entries.location
+		end 
+	end
+ 	as "bbox",
+ 	case when entries.geom is not null then
+ 		entries.geom
+ 	else
+		case when spatial_scales.extent is not null then
+			spatial_scales.extent
+		else 
+			entries.location
+		end 
+	end
+ 	as "geom"
 FROM entries
-INNER JOIN datasources ON entries.datasource_id = datasources.id
-INNER JOIN spatial_scales ON spatial_scales.id = datasources.spatial_scale_id
+LEFT JOIN datasources ON entries.datasource_id = datasources.id
+LEFT JOIN spatial_scales ON spatial_scales.id = datasources.spatial_scale_id
+ ) as t
+ 
+order by t.id asc
+limit 25
