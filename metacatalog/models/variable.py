76d2ebea15a16c4fed951b3c0733ceea9da240ac
AlexDo1 <alex.dolich@gmail.com>
2023-01-24 08:48:45 +0100

    only add keywords to dict if exists

--- metacatalog/models/variable.py
+++ metacatalog/models/variable.py
@@ -151,14 +151,12 @@ class Variable(Base):
             name=self.name,
             symbol=self.symbol,
             unit=self.unit.to_dict(deep=False),
-            column_names=self.column_names,
-            keyword=self.keyword.to_dict(deep=False) if self.keyword else None
+            column_names=self.column_names
         )
 
         # set optionals
-        for attr in ('keyword'):
-            if hasattr(self, attr) and getattr(self, attr) is not None:
-                d[attr] = getattr(self, attr).to_dict(deep=False)
+        if self.keyword:
+            d['keyword'] = self.keyword.to_dict(deep=False) 
 
         # lazy loading
         if deep:
