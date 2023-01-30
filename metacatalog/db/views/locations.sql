8a6a573b974215794a985ec09f63d408d75a0a92
AlexDo1 <alex.dolich@gmail.com>
2023-01-30 10:22:51 +0100

    updates to locations View

--- metacatalog/db/views/locations.sql
+++ metacatalog/db/views/locations.sql
@@ -1,28 +1,26 @@
---CREATE OR REPLACE VIEW locations AS
+CREATE OR REPLACE VIEW locations AS
 select 
-	st_asewkt(t.point_location),
 	t.*,
 	st_area(t.geom::geography) as "area_sqm"
 from 
 (SELECT 
 entries.id,
-	case when entries.location is not null then
-		entries.location
-	else
-		case when spatial_scales.extent is not null then
-			st_centroid(spatial_scales.extent)
-		else 
-			null::geometry
-		end
-	end  
-	as point_location
-	,
+case when entries.location is not null then
+	entries.location
+else
 	case when spatial_scales.extent is not null then
-		spatial_scales.extent
+		st_centroid(spatial_scales.extent)
 	else 
-		entries.location
-	end 
- 	as "geom"
+		null::geometry
+	end
+end  
+as point_location,
+case when spatial_scales.extent is not null then
+	spatial_scales.extent
+else 
+	entries.location
+end 
+as "geom"
 FROM entries
 LEFT JOIN datasources ON entries.datasource_id = datasources.id
 LEFT JOIN spatial_scales ON spatial_scales.id = datasources.spatial_scale_id
