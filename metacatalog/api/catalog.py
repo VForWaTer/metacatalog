76d7d19066eb921177001bee7993828a52dde125
AlexDo1 <alex.dolich@gmail.com>
2023-02-08 09:33:15 +0100

    get_uuid as_result

--- metacatalog/api/catalog.py
+++ metacatalog/api/catalog.py
@@ -4,13 +4,17 @@ The catalog API offers application wide endpoints that are not bound to a
 specific API action or model
 
 """
+from typing import Union
+
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
 
 from metacatalog import api
 from metacatalog.util.logging import get_logger
+from metacatalog.util.results import ImmutableResultSet
+from metacatalog.models import Entry, EntryGroup, Keyword, Person
 
-def get_uuid(session: Session, uuid: str, not_found='raise'):
+def get_uuid(session: Session, uuid: str, as_result: bool=False, not_found: str='raise') -> Union[Entry, EntryGroup, Keyword, Person, ImmutableResultSet, None]:
     """
     Return the Metacatalog object of given
     version 4 UUID. The supported objects are:
@@ -25,17 +29,47 @@ def get_uuid(session: Session, uuid: str, not_found='raise'):
     .. versionchanged:: 0.2.7
         Now, also :class:`Persons <metacatalog.model.Person` can be
         found by UUID
+    
+    .. versionchanged:: 0.7.5
+        Found Entry and EntryGroup can be returned as ImmutableResultSet
+        now.
+
+    Parameters
+    ----------
+    session : sqlalchemy.Session
+        SQLAlchemy session connected to the database.
+    uuid : str
+        Find by version 4 UUID.
+    as_result : bool
+        .. versionadded:: 0.7.5
+        
+        If True, the found Entry or Entrygroup will be merged into a
+        :class:`ImmutableResultSet <metacatalog.util.results.ImmutableResultSet>`.
+        Ignored for matched Keyword and Person.
+        Defaults to False.
+
+    Returns
+    -------
+    record : Entry, EntryGroup, Keyword, Person, ImmutableResultSet, None
+        Matched Entry, EntryGroup, Keyword, Person or ImmutableResultSet.
+        If no match was found, None is returned.
 
     """
     # check if an Entry exists
     entry = api.find_entry(session, uuid=uuid)
     if entry is not None:
-        return entry
+        if as_result:
+            return ImmutableResultSet(entry)
+        else:
+            return entry
 
     # check if Entrygroup exists
     group = api.find_group(session, uuid=uuid)
     if group is not None:
-        return group
+        if as_result:
+            return ImmutableResultSet(group)
+        else:
+            return group
 
     # check if a Person exists
     person = api.find_person(session, uuid=uuid)
