28ba020f16f046dd779a5cb89d22405ad55af9d9
AlexDo1 <alex.dolich@gmail.com>
2023-02-15 13:48:12 +0100

    wip

--- metacatalog/ext/standards_export/extension.py
+++ metacatalog/ext/standards_export/extension.py
@@ -46,8 +46,8 @@ class StandardsExportExtension(MetacatalogExtensionInterface):
         Entry.standards_export = wrapper_entry
 
         # add function create_iso19115 to api.catalog
-        def wrapper_api(session: Session, config_dict: dict, path: str, if_exists: str = 'fail', verbose: bool = False):
-            StandardsExportExtension.create_standard_metadata(session, config_dict, path, if_exists, verbose)
+        def wrapper_api(session: Session, id_or_uuid: Union[int, str], config_dict: dict, path: str = None, template_path: str = './schemas/iso19115/iso19115-2.j2'):
+            StandardsExportExtension.create_standard_metadata(session, id_or_uuid, config_dict, path, template_path)
 
         wrapper_api.__doc__ = StandardsExportExtension.create_standard_metadata.__doc__
         wrapper_api.__name__ = StandardsExportExtension.create_standard_metadata.__name__
@@ -145,109 +145,109 @@ class StandardsExportExtension(MetacatalogExtensionInterface):
     @overload
     def create_standard_metadata(path: Literal[None]) -> ElementTree: ...
     def create_standard_metadata(session: Session, id_or_uuid: Union[int, str], config_dict: dict, path: str = None, template_path: str = './schemas/iso19115/iso19115-2.j2') -> ElementTree | None:
-            """
-            This function can be imported from metacatalog.api.catalog
+        """
+        This function can be imported from metacatalog.api.catalog
 
-            Create standard metadata XML file for an entry, which is found 
-            by its id or uuid. The metadata standard is determined by the
-            jinja2 template passed in ``template_path``. Currently defaults
-            to the ISO 19115 template.
-            The XML file is saved to the folder given in ``path``. If ``path``
-            does not end with '.xml', the name of the XML file is generated 
-            with the uuid of the used ImmutableResultSet: 
-            f"iso19115_{irs_uuid}.xml" 
-            TODO: naming for other standards in the future.
+        Create standard metadata XML file for an entry, which is found 
+        by its id or uuid. The metadata standard is determined by the
+        jinja2 template passed in ``template_path``. Currently defaults
+        to the ISO 19115 template.
+        The XML file is saved to the folder given in ``path``. If ``path``
+        does not end with '.xml', the name of the XML file is generated 
+        with the uuid of the used ImmutableResultSet: 
+        f"iso19115_{irs_uuid}.xml" 
+        TODO: naming for other standards in the future.
 
-            .. versionadded:: 0.7.8
+        .. versionadded:: 0.7.8
 
-            Parameters
-            ----------
-            session : sqlalchemy.Session
-                SQLAlchemy session connected to the database.
-            id_or_uuid : Union[int, str]
-                id or uuid of the Entry to be exported.
-            config_dict : dict
-                Configuration dictionary, containing information about the data provider.
-                Mandatory (nested) keys and type of values:
+        Parameters
+        ----------
+        session : sqlalchemy.Session
+            SQLAlchemy session connected to the database.
+        id_or_uuid : Union[int, str]
+            id or uuid of the Entry to be exported.
+        config_dict : dict
+            Configuration dictionary, containing information about the data provider.
+            Mandatory (nested) keys and type of values:
 
-                .. code-block:: Python
+            .. code-block:: Python
 
-                dict(
-                    contact: dict(
-                        organisationName: str = '',
-                        deliveryPoint: str = '',
-                        city: str = '',
-                        administrativeArea: str = '',
-                        postalCode: str = '',
-                        country: str = '',
-                        electronicMailAddress: list(str) = ['', ''],
-                        linkage: str = '',
-                        linkage_name: str = '',
-                        linkage_description: str = ''
-                    ),
-                    publisher: dict(
-                        organisation_name: str = ''
-                    )
+            dict(
+                contact: dict(
+                    organisationName: str = '',
+                    deliveryPoint: str = '',
+                    city: str = '',
+                    administrativeArea: str = '',
+                    postalCode: str = '',
+                    country: str = '',
+                    electronicMailAddress: list(str) = ['', ''],
+                    linkage: str = '',
+                    linkage_name: str = '',
+                    linkage_description: str = ''
+                ),
+                publisher: dict(
+                    organisation_name: str = ''
                 )
-            path : str
-                Location where the ISO19115 XML file is saved to.
-                If path ends with the name of the XML file (i.e. ends with '.xml'), the file is
-                named as given.
-                If path is a folder location, the name of the XML file is auto-generated with
-                the uuid of the ImmutableResultSet of the entry: ``f"iso19115_{uuid}.xml".
-                If no path is given, the class:`ElementTree <xml.etree.ElementTree.ElementTree>` object
-                representing the XML ElementTree is returned.
-            template_path : str
-                Full path (including the template name) to the jinja2 template for 
-                metadata export.  
-                Currently defaults to the ISO 19115 template.
+            )
+        path : str
+            Location where the ISO19115 XML file is saved to.
+            If path ends with the name of the XML file (i.e. ends with '.xml'), the file is
+            named as given.
+            If path is a folder location, the name of the XML file is auto-generated with
+            the uuid of the ImmutableResultSet of the entry: ``f"iso19115_{uuid}.xml".
+            If no path is given, the class:`ElementTree <xml.etree.ElementTree.ElementTree>` object
+            representing the XML ElementTree is returned.
+        template_path : str
+            Full path (including the template name) to the jinja2 template for 
+            metadata export.  
+            Currently defaults to the ISO 19115 template.
 
-            Returns
-            ----------
-            xml : Union[ElementTree, None]
-                If no path is given, the :class:`ElementTree <xml.etree.ElementTree.ElementTree>` object
-                representing the XML ElementTree in Python is returned.
-                If a path is given, None is returned.
-            
-            Notes
-            ----------
-            The content of the file will be created using a 
-            :class:`ImmutableResultSet <metacatalog.utils.results.ImmutableResultSet>`.
-            This will lazy-load sibling Entries and parent groups as needed for
-            a useful Metadata export.  
+        Returns
+        ----------
+        xml : Union[ElementTree, None]
+            If no path is given, the :class:`ElementTree <xml.etree.ElementTree.ElementTree>` object
+            representing the XML ElementTree in Python is returned.
+            If a path is given, None is returned.
+        
+        Notes
+        ----------
+        The content of the file will be created using a 
+        :class:`ImmutableResultSet <metacatalog.utils.results.ImmutableResultSet>`.
+        This will lazy-load sibling Entries and parent groups as needed for
+        a useful Metadata export.  
 
-            """
-            # find the entry by id
-            if isinstance(id_or_uuid, int):
-                entry = api.find_entry(session, id=id_or_uuid, return_iterator=True).first()
-                # raise error if no entry was found
-                if not entry:
-                    raise NoResultFound(f"No entry with id={id_or_uuid} was found.")
+        """
+        # find the entry by id
+        if isinstance(id_or_uuid, int):
+            entry = api.find_entry(session, id=id_or_uuid, return_iterator=True).first()
+            # raise error if no entry was found
+            if not entry:
+                raise NoResultFound(f"No entry with id={id_or_uuid} was found.")
 
-            # find the entry by uuid
-            elif isinstance(id_or_uuid, str):
-                entry = api.find_entry(session, uuid=id_or_uuid)
-                # raise error if no entry was found
-                if not entry:
-                    raise NoResultFound(f"No entry with uuid={id_or_uuid} was found.")
+        # find the entry by uuid
+        elif isinstance(id_or_uuid, str):
+            entry = api.find_entry(session, uuid=id_or_uuid)
+            # raise error if no entry was found
+            if not entry:
+                raise NoResultFound(f"No entry with uuid={id_or_uuid} was found.")
 
-            # export entry to ISO 19115
-            xml = entry.standards_export(config_dict=config_dict, template_path=template_path)
+        # export entry to ISO 19115
+        xml = entry.standards_export(config_dict=config_dict, template_path=template_path)
 
-            if path:
-                # get the uuid of the ImmutableResultSet that is written to ISO19115 XML (rs.group.uuid or rs.get('uuid'))
-                irs_uuid = ImmutableResultSet(entry).uuid
+        if path:
+            # get the uuid of the ImmutableResultSet that is written to ISO19115 XML (rs.group.uuid or rs.get('uuid'))
+            irs_uuid = ImmutableResultSet(entry).uuid
 
-                # use absolute path
-                path = os.path.abspath(path)
+            # use absolute path
+            path = os.path.abspath(path)
 
-                # if path does not end with .xml: auto-generate XML filename
-                if not path.endswith('.xml'):
-                    path += f"/iso19115_{irs_uuid}.xml"
+            # if path does not end with .xml: auto-generate XML filename
+            if not path.endswith('.xml'):
+                path += f"/iso19115_{irs_uuid}.xml"
 
-                # write XML file
-                with open(path, 'wb') as f:
-                    xml.write(f, encoding='utf-8')
-            else:
-                return ET.ElementTree(ET.fromstring(xml))
-            
+            # write XML file
+            with open(path, 'wb') as f:
+                xml.write(f, encoding='utf-8')
+        else:
+            return xml
+        
