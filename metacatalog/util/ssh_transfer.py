3dcf672b81f139ff1522a8c07cb08b0f94fef894
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-10-27 12:49:06 +0200

    fix local copy

--- metacatalog/util/ssh_transfer.py
+++ metacatalog/util/ssh_transfer.py
@@ -10,8 +10,9 @@ from typing import Optional
 import os
 import shutil
 import glob
+from contextlib import closing
 
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 from dotenv import load_dotenv
 from tqdm import tqdm
 
@@ -20,10 +21,11 @@ load_dotenv()
 
 
 class FileTransfer:  # pragma: no cover
-    def __init__(self, host: str = 'localhost', username: Optional[str] = None, password: Optional[str] = None, quiet: bool = False):
+    def __init__(self, host: str = 'localhost', username: Optional[str] = None, password: Optional[str] = None, port: Optional[int] = 22, quiet: bool = False):
         self.host = host
         self.username = username
         self.password = password
+        self.port = port
         self.quiet = quiet
 
         # figure out if we need to use SSH
@@ -42,6 +44,25 @@ class FileTransfer:  # pragma: no cover
             self._local_copy(source, target)
         else:
             self._ssh_put(source, target)
+
+    def _connect_ssh(self) -> SSHClient:
+        # check that all necessary information has been loaded to the environment variables
+        host = self.host
+        username = self.username or os.getenv('SSH_USERNAME')
+        password = self.password or os.getenv('SSH_PASSWORD')
+        port = self.port or os.getenv('SSH_PORT')
+
+        # check that all necessary information is available
+        assert username is not None, 'No username given for SSH connection.'
+
+        # create the client
+        with closing(SSHClient()) as client:
+            client.set_missing_host_key_policy(AutoAddPolicy)
+        
+            # connect the client
+            client.connect(hostname=host, username=username, password=password, port=port)
+        
+            return client
 
     def _local_copy(self, source: str, target: str) -> None:
         if os.path.isfile(source):            
@@ -67,3 +88,4 @@ class FileTransfer:  # pragma: no cover
             # get the relative path for fname to the source
             rel_path = os.path.relpath(fname, source)
             self._local_copy(fname, os.path.join(target, rel_path))
+
